b2deffb4793a44f9323d8ba325b3a02aecc7f5b3
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-03-23 20:50:22 +0000

    Removing more BSP items

---
+++ packages/ui/themes/es-theme-art-book-next/package.mk
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020 Shanti Gilbert (https://github.com/shantigilbert)
+# Copyright (C) 2020 351ELEC team (https://github.com/fewtarius/351ELEC)
+# Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
+
+PKG_NAME="es-theme-art-book-next"
+PKG_VERSION="ca90abef8ae88c573e588436490aa2cb366062cd"
+PKG_ARCH="any"
+PKG_LICENSE="CUSTOM"
+PKG_SITE="https://github.com/anthonycaccese/art-book-next-jelos"
+PKG_URL="${PKG_SITE}.git"
+GET_HANDLER_SUPPORT="git"
+PKG_SHORTDESC="Art Book Next"
+PKG_LONGDESC="Art Book Next"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/share/themes/${PKG_NAME}
+  cp -rf * ${INSTALL}/usr/share/themes/${PKG_NAME}
+}
