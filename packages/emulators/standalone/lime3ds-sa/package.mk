a48a752f8b249e6f3f2107513d847b313d89bd56
spycat88 <spycat88@users.noreply.github.com>
2024-04-01 19:48:08 +0100

    emulators: rename lime-sa > lime3ds-sa and log to /var/log

--- packages/emulators/standalone/lime3ds-sa/package.mk
+++ packages/emulators/standalone/lime3ds-sa/package.mk
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
-PKG_NAME="lime-sa"
-PKG_VERSION="235697e41f0f28237a9dd594f94e6b9038c2d04b"
+PKG_NAME="lime3ds-sa"
+PKG_VERSION="6bd64d83b770ca5ee7d0faaba400693950367822"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/Lime3DS/Lime3DS"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain ffmpeg mesa SDL2 boost zlib libusb boost zstd control-gen"
-PKG_LONGDESC="Lime - Nintendo 3DS emulator"
+PKG_LONGDESC="Lime3DS - Nintendo 3DS emulator"
 PKG_TOOLCHAIN="cmake"
 
 if [ ! "${OPENGL}" = "no" ]; then
@@ -31,9 +31,9 @@ PKG_CMAKE_OPTS_TARGET+="-DENABLE_QT=OFF \
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
-    cp ${PKG_BUILD}/.${TARGET_NAME}/bin/MinSizeRel/lime ${INSTALL}/usr/bin
+    cp ${PKG_BUILD}/.${TARGET_NAME}/bin/MinSizeRel/lime ${INSTALL}/usr/bin/lime3ds
     cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
 
-  mkdir -p ${INSTALL}/usr/config/lime-emu
-    cp -rf ${PKG_DIR}/config/${DEVICE}/* ${INSTALL}/usr/config/lime-emu
+  mkdir -p ${INSTALL}/usr/config/lime3ds
+    cp -rf ${PKG_DIR}/config/${DEVICE}/* ${INSTALL}/usr/config/lime3ds
 }
---
+++ packages/emulators/standalone/lime3ds-sa/scripts/start_lime3ds.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+. /etc/profile
+set_kill set "-9 lime3ds"
+
+# Load gptokeyb support files
+control-gen_init.sh
+source /storage/.config/gptokeyb/control.ini
+get_controls
+
+# Make sure Lime3DS config directory exists
+[ ! -d /storage/.config/lime3ds ] && cp -r /usr/config/lime3ds /storage/.config
+
+# Move sdmc & nand to 3ds roms folder
+[ ! -d /storage/roms/3ds/lime3ds/sdmc ] && mkdir -p /storage/roms/3ds/lime3ds/sdmc
+rm -rf /storage/.config/lime3ds/sdmc
+ln -sf /storage/roms/3ds/lime3ds/sdmc /storage/.config/lime3ds/sdmc
+
+[ ! -d /storage/roms/3ds/lime3ds/nand ] && mkdir -p /storage/roms/3ds/lime3ds/nand
+rm -rf /storage/.config/lime3ds/nand
+ln -sf /storage/roms/3ds/lime3ds/nand /storage/.config/lime3ds/nand
+
+
+# Emulation Station Features
+GAME=$(echo "${1}"| sed "s#^/.*/##")
+CPU=$(get_setting cpu_speed 3ds "${GAME}")
+EMOUSE=$(get_setting emulate_mouse 3ds "${GAME}")
+RENDERER=$(get_setting graphics_backend 3ds "${GAME}")
+RES=$(get_setting resolution_scale 3ds "${GAME}")
+ROTATE=$(get_setting rotate_screen 3ds "${GAME}")
+SLAYOUT=$(get_setting screen_layout 3ds "${GAME}")
+
+# CPU Underclock
+case "${CPU}" in
+  0) sed -i '/cpu_clock_percentage =/c\cpu_clock_percentage = 100' /storage/.config/lime3ds/sdl2-config.ini;;
+  1) sed -i '/cpu_clock_percentage =/c\cpu_clock_percentage = 90' /storage/.config/lime3ds/sdl2-config.ini;;
+  2) sed -i '/cpu_clock_percentage =/c\cpu_clock_percentage = 80' /storage/.config/lime3ds/sdl2-config.ini;;
+  3) sed -i '/cpu_clock_percentage =/c\cpu_clock_percentage = 70' /storage/.config/lime3ds/sdl2-config.ini;;
+  4) sed -i '/cpu_clock_percentage =/c\cpu_clock_percentage = 60' /storage/.config/lime3ds/sdl2-config.ini;;
+  5) sed -i '/cpu_clock_percentage =/c\cpu_clock_percentage = 50' /storage/.config/lime3ds/sdl2-config.ini;;
+esac
+
+# Resolution Scale
+case "${RES}" in
+  0) sed -i '/resolution_factor =/c\resolution_factor = 0' /storage/.config/lime3ds/sdl2-config.ini;;
+  1) sed -i '/resolution_factor =/c\resolution_factor = 1' /storage/.config/lime3ds/sdl2-config.ini;;
+  2) sed -i '/resolution_factor =/c\resolution_factor = 2' /storage/.config/lime3ds/sdl2-config.ini;;
+esac
+
+# Rotate Screen
+case "${ROTATE}" in
+  0) sed -i '/upright_screen =/c\upright_screen = 0' /storage/.config/lime3ds/sdl2-config.ini;;
+  1) sed -i '/upright_screen =/c\upright_screen = 1' /storage/.config/lime3ds/sdl2-config.ini;;
+esac
+
+# Screen Layout
+case "${SLAYOUT}" in
+  0)
+    sed -i '/layout_option =/c\layout_option = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/swap_screen =/c\swap_screen = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/custom_layout =/c\custom_layout = 0' /storage/.config/lime3ds/sdl2-config.ini
+    ;;
+  1a)
+    sed -i '/layout_option =/c\layout_option = 1' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/swap_screen =/c\swap_screen = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/custom_layout =/c\custom_layout = 0' /storage/.config/lime3ds/sdl2-config.ini
+    ;;
+  1b)
+    sed -i '/layout_option =/c\layout_option = 1' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/swap_screen =/c\swap_screen = 1' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/custom_layout =/c\custom_layout = 0' /storage/.config/lime3ds/sdl2-config.ini
+    ;;
+  2)
+    sed -i '/layout_option =/c\layout_option = 2' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/swap_screen =/c\swap_screen = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/custom_layout =/c\custom_layout = 0' /storage/.config/lime3ds/sdl2-config.ini
+    ;;
+  3)
+    sed -i '/layout_option =/c\layout_option = 3' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/swap_screen =/c\swap_screen = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/custom_layout =/c\custom_layout = 0' /storage/.config/lime3ds/sdl2-config.ini
+    ;;
+  4)
+    sed -i '/layout_option =/c\layout_option = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/swap_screen =/c\swap_screen = 0' /storage/.config/lime3ds/sdl2-config.ini
+    sed -i '/custom_layout =/c\custom_layout = 1' /storage/.config/lime3ds/sdl2-config.ini
+    ;;
+esac
+
+# Video Backend
+case "${RENDERER}" in
+  1) sed -i '/graphics_api =/c\graphics_api = 1' /storage/.config/lime3ds/sdl2-config.ini;;
+  *) sed -i '/graphics_api =/c\graphics_api = 2' /storage/.config/lime3ds/sdl2-config.ini;;
+esac
+
+rm -rf /storage/.local/share/lime3ds
+ln -sf /storage/.config/lime3ds /storage/.local/share/lime3ds
+
+# Run Lime Emulator
+if [ "${EMOUSE}" = "0" ]; then
+  /usr/bin/lime3ds "${1}"
+else
+  ${GPTOKEYB} lime3ds -c /storage/.config/lime3ds/lime3ds.gptk &
+  /usr/bin/lime3ds "${1}"
+  kill -9 "$(pidof gptokeyb)"
+fi
