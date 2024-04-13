e713ee78866755e3e7510ea58e43262fbbf4f8aa
spycat88 <spycat88@users.noreply.github.com>
2024-04-14 00:08:40 +0100

    rocknix-joypad: introduce package

---
+++ packages/kernel/drivers/rocknix-joypad/package.mk
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="rocknix-joypad"
+PKG_VERSION="3f857f0f5905a565ccef58fa8231b4b511d281a1"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/ROCKNIX/rocknix-joypad"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_LONGDESC="rocknix-joypad: ROCKNIX joypad driver"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
+PKG_TOOLCHAIN="manual"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  kernel_make -C $(kernel_path) M=${PKG_BUILD}
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+}
