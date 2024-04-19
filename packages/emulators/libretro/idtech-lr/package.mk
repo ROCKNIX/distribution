c08c6ae585c3acad05a31aceaa03e6705b20daa7
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-04-19 23:01:14 +0000

    Start working on ID Tech

--- packages/emulators/libretro/idtech-lr/package.mk
+++ packages/emulators/libretro/idtech-lr/package.mk
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="idtech-lr"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://rocknix.org"
 PKG_LONGDESC="Package for all iD Software game engines."
 PKG_TOOLCHAIN="manual"
+PKG_DOOM_SHAREWARE="https://github.com/ROCKNIX/packages/raw/main/doom.tar.gz"
 
 if [[ "${OPENGL_SUPPORT}" = "yes" ]] && [[ ! "${DEVICE}" = "S922X" ]]; then
   PKG_DEPENDS_TARGET+=" ${OPENGL}"
@@ -20,3 +21,14 @@ fi
 if [ "${TARGET_ARCH}" = "x86_64" ]; then
   PKG_DEPENDS_TARGET+=" boom3-lr"
 fi
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/config/idtech
+  mkdir -p ${INSTALL}/usr/bin
+  cp -rf ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
+  chmod +x ${INSTALL}/usr/bin/*
+
+  mkdir -p ${INSTALL}/usr/share/idtech
+  cp -rf ${PKG_DIR}/sources/* ${INSTALL}/usr/share/idtech/
+  curl -Lo ${INSTALL}/usr/share/idtech/doom.tar.gz ${PKG_DOOM_SHAREWARE}
+}
---
+++ packages/emulators/libretro/idtech-lr/scripts/generate-idtech.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+
+# This scripts scans the /storage/roms/idtech folder for idtech game files
+# and creates the necessary idtech launcher scripts
+
+
+RA_BIN="/usr/bin/retroarch"
+RA_DIR="/usr/lib/libretro"
+SCRIPT_DIR="/storage/.config/idtech"
+GAME_DIR="/storage/roms/idtech"
+
+### Create directories
+while read IDTECH_DIR; do
+  if [ ! -d "${GAME_DIR}/${IDTECH_DIR}" ]; then
+    mkdir -p ${GAME_DIR}/${IDTECH_DIR}
+  fi
+done </usr/share/idtech/idtech_dirs
+
+
+### Create scripts ###
+
+### Doom / Doom shareware
+if [[ ! -f "${GAME_DIR}/doom/doom/doom.wad" ]]; then
+  tar -xf /usr/share/idtech/doom.tar.gz -C ${GAME_DIR}/doom/doom/
+fi
+
+if [[ -f "${GAME_DIR}/doom/doom/doom.wad" ]] && [[ ! -f ${SCRIPT_DIR}/Doom.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Doom.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/prboom_libretro.so ${GAME_DIR}/doom/doom/doom.wad
+EOF
+fi
+
+### Doom 2
+if [[ -f "${GAME_DIR}/doom/doom2/doom2.wad" ]] && [[ ! -f ${SCRIPT_DIR}/Doom\ II.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Doom\ II.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/prboom_libretro.so ${GAME_DIR}/doom/doom2/doom2.wad
+EOF
+fi
+
+### The Plutonia Experiment
+if [[ -f "${GAME_DIR}/doom/plutonia/plutonia.wad" ]] && [[ ! -f ${SCRIPT_DIR}/The\ Plutonia\ Experiment.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/The\ Plutonia\ Experiment.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/prboom_libretro.so ${GAME_DIR}/doom/plutonia/plutonia.wad
+EOF
+fi
+
+### TNT Evilution
+if [[ -f "${GAME_DIR}/doom/tnt/tnt.wad" ]] && [[ ! -f ${SCRIPT_DIR}/TNT\ Evilution.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/TNT\ Evilution.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/prboom_libretro.so ${GAME_DIR}/doom/tnt/tnt.wad
+EOF
+fi
+
+### SIGIL
+if [[ -f "${GAME_DIR}/doom/sigil/sigil.wad" ]] && [[ ! -f ${SCRIPT_DIR}/Sigil.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Sigil.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/prboom_libretro.so ${GAME_DIR}/doom/sigil/sigil.wad
+EOF
+fi
+
+### SIGIL II
+if [[ -f "${GAME_DIR}/doom/sigil2/sigil2.wad" ]] && [[ ! -f ${SCRIPT_DIR}/Sigil\ II.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Sigil\ II.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/prboom_libretro.so ${GAME_DIR}/doom/sigil2/sigil2.wad
+EOF
+fi
+
+### Quake
+if [[ -f "${GAME_DIR}/quake/id1/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/tyrquake_libretro.so ${GAME_DIR}/quake/id1/pak1.pak
+EOF
+fi
+
+### Quake - Dimension of the Past
+if [[ -f "${GAME_DIR}/quake/dopa/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ -\ Dimension\ of\ the\ Past.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ -\ Dimension\ of\ the\ Past.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/tyrquake_libretro.so ${GAME_DIR}/quake/dopa/pak0.pak
+EOF
+fi
+
+### Quake - Scourge of Armagon
+if [[ -f "${GAME_DIR}/quake/hipnotic/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ -\ Scourge\ of\ Armagon.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ -\ Scourge\ of\ Armagon.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/tyrquake_libretro.so ${GAME_DIR}/quake/hipnotic/pak0.pak
+EOF
+fi
+
+### Quake - Dissolution of Eternity
+if [[ -f "${GAME_DIR}/quake/rogue/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ -\ Dissolution\ of\ Eternity.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ -\ Dissolution\ of\ Eternity.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/tyrquake_libretro.so ${GAME_DIR}/quake/rogue/pak0.pak
+EOF
+fi
+
+### Quake 2
+if [[ -f "${GAME_DIR}/quake2/quake2/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ II.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ II.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/vitaquake2_libretro.so ${GAME_DIR}/quake2/quake2/*
+EOF
+fi
+
+### Quake 2 - The Reckoning
+if [[ -f "${GAME_DIR}/quake2/xatrix/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ II\ -\ The\ Reckoning.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ II\ -\ The\ Reckoning.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/vitaquake2_libretro.so ${GAME_DIR}/quake2/xatrix/*
+EOF
+fi
+
+### Quake 2 - Ground Zero
+if [[ -f "${GAME_DIR}/quake2/rogue/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ II\ -\ Ground\ Zero.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ II\ -\ Ground\ Zero.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/vitaquake2_libretro.so ${GAME_DIR}/quake2/rogue/*
+EOF
+fi
+
+### Quake 3
+if [[ -f "${GAME_DIR}/quake3/pak0.pak" ]] && [[ ! -f ${SCRIPT_DIR}/Quake\ III.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Quake\ III.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/vitaquake3_libretro.so ${GAME_DIR}/quake3/*
+EOF
+fi
+
+### Wolf3D
+if [[ -f "${GAME_DIR}/wolf3d/wolf3d/VSWAP.WL6" ]] && [[ ! -f ${SCRIPT_DIR}/Wolfenstein\ 3D.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Wolfenstein\ 3D.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/ecwolf_libretro.so ${GAME_DIR}/wolf3d/wolf3d/*
+EOF
+fi
+
+### Wolf3D - Spear of Destiny
+if [[ -f "${GAME_DIR}/wolf3d/sod/VSWAP.SOD" ]] && [[ ! -f ${SCRIPT_DIR}/Wolfenstein\ 3D\ -\ Spear\ of\ Destiny.sh ]]; then
+cat <<EOF >${SCRIPT_DIR}/Wolfenstein\ 3D\ -\ Spear\ of\ Destiny.sh
+#!/bin/bash
+
+${RA_BIN} -L ${RA_DIR}/ecwolf_libretro.so ${GAME_DIR}/wolf3d/sod/*
+EOF
+fi
+
+
+### Set all launcher scripts to be executable ###
+chmod +x ${SCRIPT_DIR}/*
