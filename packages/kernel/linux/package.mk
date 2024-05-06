2cde972364450ef7cdc8bd1c562d9f34947b386c
spycat88 <spycat88@users.noreply.github.com>
2024-05-06 14:28:32 +0100

    powertop: remove package and cleanup it's dependencies

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -44,7 +44,7 @@ fi
 
 if [ "${PKG_BUILD_PERF}" != "no" ] && grep -q ^CONFIG_PERF_EVENTS= ${PKG_KERNEL_CFG_FILE}; then
   PKG_BUILD_PERF="yes"
-  PKG_DEPENDS_TARGET+=" binutils elfutils libunwind zlib openssl libtraceevent libtracefs"
+  PKG_DEPENDS_TARGET+=" binutils elfutils libunwind zlib openssl"
 fi
 
 if [[ "${TARGET_ARCH}" =~ i*86|x86_64 ]]; then
