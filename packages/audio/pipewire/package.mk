1d41280b7cf63cb9d84bc3ca1ce7533e8e7cc238
Paul Reioux <reioux@gmail.com>
2024-05-09 08:15:07 -0700

    pipewire: create 2 virtual audio devices and add user config for them
    added mono and stereo virtual audio devices, this will allow handhelds
    such as the PowKiddy SX20 and others with just 1 mono speaker to downmix
    all stereo output for the single mono speaker so for games with channel
    specific audios, you miss out any audio while playing.  This is a
    software only solution which allows greater flexibility to allow future
    manipulations of audio without touching the original HW configurations
    
    also adding the shell scripts to swap between the virtual devices
    later during ES operations
    
    pipewire: make virtual audio optional only for RK3326 and RK3566
    platforms
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    pipewire: enable virtual audio drivers for all rocknix devices
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- packages/audio/pipewire/package.mk
+++ packages/audio/pipewire/package.mk
@@ -89,6 +89,12 @@ post_install() {
   mkdir -p ${INSTALL}/etc/alsa/conf.d
   ln -sf /usr/share/alsa/alsa.conf.d/50-pipewire.conf ${INSTALL}/etc/alsa/conf.d/50-pipewire.conf
   ln -sf /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf ${INSTALL}/etc/alsa/conf.d/99-pipewire-default.conf
+
+  mkdir -p ${INSTALL}/etc/pipewire/pipewire.conf.d/
+  cp ${PKG_DIR}/sources/audio-v-devs.conf ${INSTALL}/etc/pipewire/pipewire.conf.d/
+  cp ${PKG_DIR}/sources/audio_vdriver.sh ${INSTALL}/usr/bin/
+  chmod 0755 ${INSTALL}/usr/bin/audio_vdriver.sh
+
   enable_service pipewire.socket
   enable_service pipewire.service
   enable_service pipewire-pulse.socket
