d758051a38755dc4d4b85fa3493ee21668a8ba12
spycat88 <spycat88@users.noreply.github.com>
2024-05-13 12:50:55 +0100

    linux: bump to 6.9 for RK3566/RK3399/S922X

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -34,8 +34,8 @@ case ${DEVICE} in
     PKG_URL="https://git.sr.ht/~tokyovigilante/linux/archive/${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.9-rc7"
-    PKG_URL="https://git.kernel.org/torvalds/t/linux-${PKG_VERSION}.tar.gz"
+    PKG_VERSION="6.9"
+    PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
 esac
