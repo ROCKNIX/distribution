301278fcbefc704d9fba877e2720783272fb54e3
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-05-16 15:08:42 +0000

    Bump rocknix joypad

--- packages/kernel/drivers/rocknix-joypad/package.mk
+++ packages/kernel/drivers/rocknix-joypad/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-joypad"
-PKG_VERSION="b7b874fdb37fdc88a7e89b6e2ccc93eb9b27c5d2"
+PKG_VERSION="72072b2079094fa7cce5aba651826e9c6ae90de0"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/ROCKNIX/rocknix-joypad"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
