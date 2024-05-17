665c3e3b38a3e11a9a9736c05870812c514d4aec
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-05-17 21:06:41 +0000

    Bump Kernel to 6.9.1

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -34,7 +34,7 @@ case ${DEVICE} in
     PKG_URL="https://git.sr.ht/~tokyovigilante/linux/archive/${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.9"
+    PKG_VERSION="6.9.1"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
