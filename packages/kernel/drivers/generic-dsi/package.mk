366645858e4958743c11d1a154455503fe3f411f
Danil Zagoskin <z@gosk.in>
2024-06-01 01:09:11 +0300

    generic-dsi driver: fix buffer allocation, support read commands

--- packages/kernel/drivers/generic-dsi/package.mk
+++ packages/kernel/drivers/generic-dsi/package.mk
@@ -11,9 +11,11 @@ PKG_TOOLCHAIN="manual"
 ### For development. Easier to check if code compiles and works.
 ### No need for a long build-transfer-reboot-wait-reboot-check loop
 make_target() {
-  if [ "${I_AM_DEVELOPER}" != "yes" ]; then
-    echo "######   you should not build ${PKG_NAME} as package!  ######" >&2;
-    exit 3;
+  if [ "${I_AM_DEVELOPER}" == "yes" ]; then
+    # rename the driver to not conflict with built-in one
+    sed 's|define DRIVER_NAME .*$|define DRIVER_NAME "panel-generic-dsi-test"|' panel-generic-dsi.c > panel-generic-dsi-test.c
+    echo 'obj-m += panel-generic-dsi-test.o' > ${PKG_BUILD}/Makefile
+    kernel_make -C $(kernel_path) M=${PKG_BUILD}
   fi
   echo 'obj-m += panel-generic-dsi.o' > ${PKG_BUILD}/Makefile
   kernel_make -C $(kernel_path) M=${PKG_BUILD}
