3c61913c2baf6ef4c20223bd97679311e81411b4
Nicholas Ricciuti <rishooty@gmail.com>
2024-06-21 16:29:52 -0400

    very close to compiling gtkmm

---
+++ packages/devel/glibmm-2.4/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present Nicholas Ricciuti (https://github.com/rishooty)
+
+# No package 'pangomm-1.4' found
+PKG_NAME="glibmm-2.4"
+PKG_VERSION="2.66.7"
+PKG_SHA256="fe02c1e5f5825940d82b56b6ec31a12c06c05c1583cfe62f934d0763e1e542b3"
+PKG_LICENSE="LGPL-2.1-or-later"
+PKG_SITE="https://www.gtkmm.org/"
+PKG_URL="https://download.gnome.org/sources/glibmm/${PKG_VERSION%.*}/glibmm-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="glibmm is the official C++ interface for glib."
