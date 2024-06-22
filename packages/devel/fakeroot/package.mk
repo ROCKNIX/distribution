1521391440813cf41d983f2a309fca1fb7f9b8c4
Max Engel <max.engel@me.com>
2024-06-22 19:14:35 -0400

    bump fakeroot and bdf2psf to latest versions so builds generate properly

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fakeroot"
-PKG_VERSION="1.34"
+PKG_VERSION="1.35"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://salsa.debian.org/clint/fakeroot"
 PKG_URL="http://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_${PKG_VERSION}.orig.tar.gz"
