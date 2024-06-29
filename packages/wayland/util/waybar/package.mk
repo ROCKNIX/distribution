2f03edb5e7411022d19af96c3ef48c7684a3ba3e
Nicholas Ricciuti <rishooty@gmail.com>
2024-06-29 00:17:04 -0400

    fix service conflict, fix missing dependencies, tweak config to get it to overlay over fullscreen

--- packages/wayland/util/waybar/package.mk
+++ packages/wayland/util/waybar/package.mk
@@ -7,11 +7,13 @@ PKG_SHA256="50a9ae85d3dcfef04e4bc4e0f3470f187964e4466c156e5558850cea84a3df5c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/Alexays/Waybar"
 PKG_URL="https://github.com/Alexays/Waybar/archive/refs/tags/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain jsoncpp gtk3 libsigcpp libinput libxkbcommon pixman pango cairo glib libfmt spdlog gtkmm libnl pipewire pulseaudio wireplumber gtk-layer-shell"
+PKG_DEPENDS_TARGET="toolchain jsoncpp gtk3 libsigcpp libinput libxkbcommon pixman pango cairo glib libfmt spdlog gtkmm libnl pipewire pulseaudio wireplumber gtk-layer-shell font-awesome"
 PKG_LONGDESC="Waybar is a highly customizable Wayland bar for Sway and other Wlroots-based compositors."
 
 PKG_MESON_OPTS_TARGET="-Dman-pages=disabled \
-                       -Dtests=disabled"
+                       -Dtests=disabled \
+                       -Dcava=disabled \
+                       -Drfkill=enabled"
 
 pre_configure_target() {
   export TARGET_CFLAGS=$(echo "${TARGET_CFLAGS} -Wno-error=switch")
@@ -23,6 +25,6 @@ post_makeinstall_target() {
   cp ${PKG_DIR}/config/style.css ${INSTALL}/etc/xdg/waybar
 }
 
-post_install() {
-  enable_service waybar.service
-}
+# post_install() {
+#   enable_service waybar.service
+# }
