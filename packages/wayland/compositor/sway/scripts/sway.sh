cbfb6d3852c4d97ab7da853e22c716d761f654fe
Nicholas Ricciuti <rishooty@gmail.com>
2024-06-29 12:52:33 -0400

    swaybar init fixed but having trouble overlaying over fs apps

--- packages/wayland/compositor/sway/scripts/sway.sh
+++ packages/wayland/compositor/sway/scripts/sway.sh
@@ -2,6 +2,16 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
+eval $(dbus-launch --sh-syntax)
+export LANG=en_US.UTF-8
+
+#Check if waybar exists in .config
+if [ ! -d "/storage/.config/waybar" ]; then
+    mkdir -p "/storage/.config/waybar"
+    cp /etc/xdg/waybar/config.jsonc /storage/.config/waybar/config.jsonc
+    cp /etc/xdg/waybar/style.css /storage/.config/waybar/style.css
+fi
+
 . /run/sway/sway-daemon.conf
 SWAY_LOG_FILE=/var/log/sway.log
 
