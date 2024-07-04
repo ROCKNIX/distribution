09bb8c32393ac59f48865746ca4665bf6a5b053f
Danil Zagoskin <z@gosk.in>
2024-07-04 20:10:01 +0000

    bump mainline linux to 6.10-rc6

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -34,8 +34,9 @@ case ${DEVICE} in
     PKG_URL="https://git.sr.ht/~tokyovigilante/linux/archive/${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.9.7"
-    PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+    PKG_VERSION="6.10-rc6"
+    PKG_URL="https://github.com/torvalds/linux/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+    #PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
 esac
