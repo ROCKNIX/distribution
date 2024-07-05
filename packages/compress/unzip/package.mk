f2ce11a1541f09d13d077e25bdb76e0a0329b620
Nicholas Ricciuti <rishooty@gmail.com>
2024-07-05 17:21:40 -0400

    add centering, hopefully will fix mupen64plus rice

--- packages/compress/unzip/package.mk
+++ packages/compress/unzip/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="unzip"
 PKG_VERSION="60"
-PKG_SHA256="036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
+PKG_SHA256="002da7dccff8314cc6077794343990353935efbd3623a3c457ed0d8a0c1fbea1"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.info-zip.org/pub/infozip/"
 PKG_URL="https://github.com/madler/unzip/archive/0b82c20ac7375b522215b567174f370be89a4b12.tar.gz"
