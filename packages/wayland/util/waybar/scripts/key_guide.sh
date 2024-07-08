d07114ec05c0c0d912c4968a2cdc259e850e238c
Nicholas Ricciuti <rishooty@gmail.com>
2024-07-07 21:25:47 -0400

    fix function b boolean logic and add actual output

--- packages/wayland/util/waybar/scripts/key_guide.sh
+++ packages/wayland/util/waybar/scripts/key_guide.sh
@@ -12,7 +12,7 @@ FN_B_PRESSED=$(cat "$FN_B_PRESSED_FILE")
 
 if [ "$FN_A_PRESSED" = "true" ] && [ "$FN_B_PRESSED" = "false" ]; then
     echo "{\"text\":\"$KEY_GUIDE_1\", \"class\":\"key-guide-1\"}"
-elif [ "$FN_B_PRESSED" = "true" ] && [ "$FN_A_PRESSED" = "true" ]; then
+elif [ "$FN_A_PRESSED" = "false" ] && [ "$FN_B_PRESSED" = "true" ]; then
     echo "{\"text\":\"$KEY_GUIDE_2\", \"class\":\"key-guide-2\"}"
 elif [ "$FN_A_PRESSED" = "true" ] && [ "$FN_B_PRESSED" = "true" ]; then
     echo "{\"text\":\"$KEY_GUIDE_3\", \"class\":\"key-guide-3\"}"
