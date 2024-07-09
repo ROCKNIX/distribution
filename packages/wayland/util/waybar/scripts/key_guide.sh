b263c53688446f87db291c6c2acbf30579af6ce1
Nicholas Ricciuti <rishooty@gmail.com>
2024-07-08 20:39:22 -0400

    source the vars directly

--- packages/wayland/util/waybar/scripts/key_guide.sh
+++ packages/wayland/util/waybar/scripts/key_guide.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /storage/.config/profile.d/092-hotkeys
 
 KEY_GUIDE_NONE=$(echo $KEY_GUIDE_NONE)
 KEY_GUIDE_1=$(echo $KEY_GUIDE_1)
@@ -7,15 +8,15 @@ KEY_GUIDE_3=$(echo $KEY_GUIDE_3)
 
 FN_A_PRESSED_FILE="/tmp/FN_A_PRESSED.state"
 FN_B_PRESSED_FILE="/tmp/FN_B_PRESSED.state"
-FN_A_PRESSED=$(cat "$FN_A_PRESSED_FILE")
-FN_B_PRESSED=$(cat "$FN_B_PRESSED_FILE")
+FN_A_PRESSED=$(cat "$FN_A_PRESSED_FILE" 2>/dev/null || echo "false")
+FN_B_PRESSED=$(cat "$FN_B_PRESSED_FILE" 2>/dev/null || echo "false")
 
 if [ "$FN_A_PRESSED" = "true" ] && [ "$FN_B_PRESSED" = "false" ]; then
-    echo "{\"text\":\"$KEY_GUIDE_1\", \"class\":\"key-guide-1\"}"
-elif [ "$FN_B_PRESSED" = "true" ] && [ "$FN_A_PRESSED" = "true" ]; then
-    echo "{\"text\":\"$KEY_GUIDE_2\", \"class\":\"key-guide-2\"}"
+    echo "{\"text\": \"$KEY_GUIDE_1\", \"class\": \"key-guide-1\"}"
+elif [ "$FN_A_PRESSED" = "false" ] && [ "$FN_B_PRESSED" = "true" ]; then
+    echo "{\"text\": \"$KEY_GUIDE_2\", \"class\": \"key-guide-2\"}"
 elif [ "$FN_A_PRESSED" = "true" ] && [ "$FN_B_PRESSED" = "true" ]; then
-    echo "{\"text\":\"$KEY_GUIDE_3\", \"class\":\"key-guide-3\"}"
+    echo "{\"text\": \"$KEY_GUIDE_3\", \"class\": \"key-guide-3\"}"
 else
-    echo "{\"text\":\"$KEY_GUIDE_NONE\", \"class\":\"key-guide-none\"}"
+    echo "{\"text\": \"$KEY_GUIDE_NONE\", \"class\": \"key-guide-none\"}"
 fi
