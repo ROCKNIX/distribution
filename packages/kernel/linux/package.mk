4b373cdec64594f81b64dfb4835598bf50176569
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-07-12 13:35:21 +0000

    Add initial support for the Aya Neo Flip DS & bump kernel

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -39,7 +39,7 @@ case ${DEVICE} in
     PKG_PATCH_DIRS+=" mainline"
     ;;
   *)
-    PKG_VERSION="6.9.8"
+    PKG_VERSION="6.9.9"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
