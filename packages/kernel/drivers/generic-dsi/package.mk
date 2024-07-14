2641f2e428076ccb9f453ed973dc59b05ea74a95
Danil Zagoskin <z@gosk.in>
2024-07-14 18:48:27 +0300

    generic-dsi: include import script in firmware

--- packages/kernel/drivers/generic-dsi/package.mk
+++ packages/kernel/drivers/generic-dsi/package.mk
@@ -5,8 +5,8 @@ PKG_NAME="generic-dsi"
 PKG_VERSION="0.1.0"
 PKG_LICENSE="GPL"
 PKG_LONGDESC="generic DSI panel driver"
+PKG_DEPENDS_TARGET="pyFDT"
 PKG_TOOLCHAIN="manual"
-
 
 ### For development. Easier to check if code compiles and works.
 ### No need for a long build-transfer-reboot-wait-reboot-check loop
@@ -17,12 +17,11 @@ make_target() {
     echo 'obj-m += panel-generic-dsi-test.o' > ${PKG_BUILD}/Makefile
     kernel_make -C $(kernel_path) M=${PKG_BUILD}
   fi
-  echo 'obj-m += panel-generic-dsi.o' > ${PKG_BUILD}/Makefile
-  kernel_make -C $(kernel_path) M=${PKG_BUILD}
 }
 
 makeinstall_target() {
-  :
+  mkdir -p "${INSTALL}/usr/libexec/generic-dsi"
+  cp -v "${PKG_DIR}/scripts/importpanel.py" "${INSTALL}/usr/libexec/generic-dsi/"
 }
 
 
