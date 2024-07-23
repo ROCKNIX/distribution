c18b8bffcfe518f875316a0fc85fa2d15ca04627
sydarn <sydarn@proton.me>
2024-07-23 20:21:47 +0200

    RK3566: Bump linux to 6.10

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -34,8 +34,8 @@ case ${DEVICE} in
     PKG_URL="https://git.sr.ht/~tokyovigilante/linux/archive/${PKG_VERSION}.tar.gz"
     ;;
   RK3566)
-    PKG_VERSION="6.10-rc6"
-    PKG_URL="https://github.com/torvalds/linux/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+    PKG_VERSION="6.10"
+    PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
   *)
