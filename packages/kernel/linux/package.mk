48d6d4928344de0147f939ee1988dd6788a5bb6f
John Williams <porschemad911@gmail.com>
2024-08-05 23:15:43 +1000

    Revert "Merge pull request #394 from spycat88/amlogic-6.10"
    This reverts commit d6b4f7d81168284d855c3d5afe6c3f040cca72de, reversing
    changes made to 9dcbe718d1d863cb7bf0de9d8b6813f1cad39333.

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -34,7 +34,7 @@ case ${DEVICE} in
     PKG_URL="https://git.sr.ht/~tokyovigilante/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS+=" mainline"
     ;;
-  RK3566|S922X)
+  RK3566)
     PKG_VERSION="6.10"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" mainline"
