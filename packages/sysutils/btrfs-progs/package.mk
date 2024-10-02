e43957cc0e7ab6e65ab9b01d9f5ce106b0cbd1a3
Joel Pauling (aenertia) <aenertia@aenertia.net>
2024-10-03 07:43:33 +1300

    Removed unused cruft from package.mk for btrfs per review.

--- packages/sysutils/btrfs-progs/package.mk
+++ packages/sysutils/btrfs-progs/package.mk
@@ -4,21 +4,15 @@
 
 PKG_NAME="btrfs-progs"
 PKG_VERSION="6.10.1"
-PKG_SHA256="ce7f1d1c33bf5b3acd418466e7e412026e435b05f187e779a1c5303ebf1b1f96"
-PKG_REV="0"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://btrfs.readthedocs.io/"
 PKG_URL="https://github.com/kdave/btrfs-progs/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain e2fsprogs util-linux zlib systemd lzo"
-PKG_SECTION="tools"
-PKG_SHORTDESC="Tools for the btrfs filesystem"
 PKG_LONGDESC="Tools for the btrfs filesystem"
 PKG_TOOLCHAIN="configure"
 
 PKG_BUILD_FLAGS="-sysroot"
 
-PKG_ADDON_NAME="BTRFS Tools"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-backtrace \
                            --disable-documentation \
