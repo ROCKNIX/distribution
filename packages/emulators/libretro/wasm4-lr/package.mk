cdc3f0a7f9f3c111e821a58e4e51b386e9c68cd7
Joel Pauling (aenertia) <aenertia@aenertia.net>
2024-11-06 10:06:40 +0000

    New libretro core wasm-4: tested on rk3566/sd865 	new file:   packages/emulators/libretro/wasm4-lr/package.mk
    Updates Documentation and ES Systems for WASM-4
    
    	new file:   config/emulators/wasm4.conf
    	modified:   packages/virtual/emulators/package.mk

---
+++ packages/emulators/libretro/wasm4-lr/package.mk
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+
+PKG_NAME="wasm4-lr"
+PKG_VERSION="979be845216ee9d613cb6555fb8b11c01bec39a0"
+PKG_LICENSE="ISC"
+PKG_SITE="https://github.com/aduros/wasm4"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="WASM-4 is a low-level fantasy game console for building small games with WebAssembly. Game cartridges (ROMs) are small, self-contained .wasm files that can be built with any programming language that compiles to WebAssembly."
+GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN='manual'
+PKG_CMAKE_OPTS_TARGET="-DBUILD_TARGET=wasm4_libretro \
+                       -DCMAKE_BUILD_TYPE=Release"
+
+make_target() {
+  cd ${PKG_BUILD}/runtimes/native
+  cmake -B build
+  cmake --build build --target wasm4_libretro
+}
+
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_BUILD}/runtimes/native/build/wasm4_libretro.so ${INSTALL}/usr/lib/libretro/
+}
