c317738d095bb4676faeb009ff3a18b6a49f408a
Danil Zagoskin <z@gosk.in>
2024-11-24 12:59:42 +0000

    RK3566: enable RGA and update librga

--- packages/graphics/librga/package.mk
+++ packages/graphics/librga/package.mk
@@ -2,11 +2,17 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="librga"
-PKG_VERSION="ccfec13d6c48e3f0c7f24810b3dac162c40cdda8"
-PKG_ARCH="arm aarch64"
+PKG_VERSION="v2.2.0-1-b5fb3a6"
 PKG_LICENSE="Apache-2.0"
 PKG_DEPENDS_TARGET="toolchain libdrm"
-PKG_SITE="https://github.com/varphone/rockchip-linux-rga"
-PKG_URL="${PKG_SITE}.git"
+PKG_SITE="https://github.com/tsukumijima/librga-rockchip"
+PKG_URL="${PKG_SITE}/archive/refs/tags/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="RGA is an independent 2D hardware acceleration userspace driver"
 PKG_TOOLCHAIN="meson"
+
+PKG_MESON_OPTS_TARGET="-Dlibdrm=true" # for samples add "-Dlibrga_demo=true"
+
+post_unpack() {
+  grep -rl '#define LOCAL_FILE_PATH' ${PKG_BUILD}/samples | xargs -n1 sed -i 's|#define LOCAL_FILE_PATH .*$|#define LOCAL_FILE_PATH "/storage/rga"|'
+  sed -i '/utils\/3rdparty\/libdrm/d' ${PKG_BUILD}/meson.build
+}
