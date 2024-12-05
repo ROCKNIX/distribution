a5d3fd76e1315fb3f690b7e84f30f2615b637998
rmake <macebrooks@gmail.com>
2024-12-05 15:14:05 +0000

    Update kernel to 6.12.2

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -23,7 +23,7 @@ case ${DEVICE} in
     PKG_PATCH_DIRS="${LINUX} ${DEVICE} default"
   ;;
   SD865|H700|AMD64)
-    PKG_VERSION="6.12.1"
+    PKG_VERSION="6.12.2"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
   ;;
   *)
