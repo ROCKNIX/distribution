ae59ceed4541fc5746306cff3559297720d0597d
sydarn <sydarn@proton.me>
2024-12-16 09:19:37 +0100

    waffle: package
    Fix wayland-scanner pkgconfig

--- packages/virtual/image/package.mk
+++ packages/virtual/image/package.mk
@@ -42,7 +42,7 @@ else
   PKG_DEPENDS_TARGET+=" ${PKG_TOOLS} ${PKG_FONTS} ${PKG_SOUND} ${PKG_SYNC} ${PKG_GRAPHICS} ${PKG_UI} ${PKG_UI_TOOLS} ${PKG_MULTIMEDIA} misc-packages"
 
   # GL demos and tools
-  [ "${OPENGL_SUPPORT}" = "yes" ] && PKG_DEPENDS_TARGET+=" mesa-demos"
+  [ "${OPENGL_SUPPORT}" = "yes" ] && PKG_DEPENDS_TARGET+=" mesa-demos waffle"
 
   # GLmark2
   [[ ! -z "${OPENGLES_SUPPORT}" ]] && PKG_DEPENDS_TARGET+=" glmark2"
