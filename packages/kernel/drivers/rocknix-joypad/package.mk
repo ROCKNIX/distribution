18921e85c4e30df89fb4ea7a047751daa0ebc939
spycat88 <spycat88@users.noreply.github.com>
2025-01-04 17:43:17 +0000

    rocknix-joypad: bump package

--- packages/kernel/drivers/rocknix-joypad/package.mk
+++ packages/kernel/drivers/rocknix-joypad/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-joypad"
-PKG_VERSION="75177baac04aa54464690e74bdc1dbe2645111b3"
+PKG_VERSION="1e912c1f1e5355a1198f573aa862089aa0fdbc16"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/ROCKNIX/rocknix-joypad"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
