80fef3acb8067f5ae80999d1f60016b8ea29c954
rmake <macebrooks@gmail.com>
2025-01-05 05:54:59 +0000

    Update Retroarch to official 1.20 release commit

--- packages/emulators/libretro/retroarch/package.mk
+++ packages/emulators/libretro/retroarch/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="retroarch"
-PKG_VERSION="953a2d011d93e9a58f3a9b8923115a78541e10e5" # v1.20.0
+PKG_VERSION="864ad02177e5e39c9951082e127efcc6a693b1e5" # v1.20.0
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="${PKG_SITE}.git"
 PKG_LICENSE="GPLv3"
