f3e7408b1dcde28f959b0676457eae22cf24e64d
Bumblebrow <wesley.fielder@gmail.com>
2025-10-28 10:40:41 -0500

    Added eden-sa switch emulation for SM8250

---
+++ projects/ROCKNIX/packages/emulators/standalone/eden-sa/package.mk
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="eden-sa"
+PKG_LICENSE="GPLv3"
+PKG_LONGDESC="Eden is the world's most popular open-source Nintendo Switch emulator, forked from the Yuzu emulator."
+PKG_TOOLCHAIN="manual"
+PKG_SITE="https://github.com/pflyly/eden-nightly"
+PKG_VERSION="2025-10-26-27884"
+PKG_REL_VERSION="27884"
+
+case ${TARGET_ARCH} in
+  x86_64)
+    PKG_URL="${PKG_SITE}/releases/download/${PKG_VERSION}/Eden-${PKG_REL_VERSION}-Common-PGO-x86_64.AppImage"
+  ;;
+  aarch64)
+    PKG_URL="${PKG_SITE}/releases/download/${PKG_VERSION}/Eden-${PKG_REL_VERSION}-Linux-PGO-aarch64.AppImage"
+  ;;
+esac
+
+makeinstall_target() {
+  export STRIP=true
+  mkdir -p ${INSTALL}/usr/bin
+  cp ${PKG_BUILD}/${PKG_NAME}-${PKG_VERSION}.AppImage ${INSTALL}/usr/bin/eden
+  cp -rf ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
+  chmod 755 ${INSTALL}/usr/bin/*
+  mkdir -p ${INSTALL}/usr/config/eden
+  cp -rf ${PKG_DIR}/config/${DEVICE}/* ${INSTALL}/usr/config/eden/
+}
+
