b4f3731d795fc20c65c13b3ed687e57fe0f67edb
spycat88 <spycat88@users.noreply.github.com>
2025-11-19 22:55:06 +0000

    openssl: link against upstream package

--- projects/ROCKNIX/packages/security/openssl/package.mk
+++ projects/ROCKNIX/packages/security/openssl/package.mk
@@ -1,78 +1,7 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
-PKG_NAME="openssl"
-PKG_VERSION="3.3.2"
-PKG_LICENSE="Apache-2.0"
-PKG_SITE="https://openssl-library.org"
-PKG_URL="https://github.com/openssl/openssl/releases/download/${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="The Open Source toolkit for Secure Sockets Layer and Transport Layer Security"
-PKG_TOOLCHAIN="configure"
-PKG_BUILD_FLAGS="+local-cc"
-
-PKG_CONFIGURE_OPTS_SHARED="--libdir=lib \
-                           shared \
-                           threads \
-                           no-md2 \
-                           no-rc5 \
-                           no-rfc3779 \
-                           no-unit-test \
-                           no-zlib \
-                           no-zlib-dynamic \
-                           no-static-engine"
-
-PKG_CONFIGURE_OPTS_HOST="--prefix=${TOOLCHAIN} \
-                         --openssldir=${TOOLCHAIN}/etc/ssl"
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
-                           --openssldir=/etc/ssl"
-
-post_unpack() {
-  find ${PKG_BUILD}/apps -type f | xargs -n 1 -t sed 's|./demoCA|/etc/ssl|' -i
-}
-
-pre_configure_host() {
-  mkdir -p ${PKG_BUILD}/.${HOST_NAME}
-  cp -a ${PKG_BUILD}/* ${PKG_BUILD}/.${HOST_NAME}/
-}
-
-configure_host() {
-  cd ${PKG_BUILD}/.${HOST_NAME}
-  ./Configure ${PKG_CONFIGURE_OPTS_HOST} ${PKG_CONFIGURE_OPTS_SHARED} linux-${MACHINE_HARDWARE_NAME} ${CFLAGS} ${LDFLAGS}
-}
-
-makeinstall_host() {
-  make install_sw
-}
-
-pre_configure_target() {
-  mkdir -p ${PKG_BUILD}/.${TARGET_NAME}
-  cp -a ${PKG_BUILD}/* ${PKG_BUILD}/.${TARGET_NAME}/
-
-  case ${TARGET_ARCH} in
-    x86_64)
-      OPENSSL_TARGET=linux-x86_64
-      PLATFORM_FLAGS=enable-ec_nistp_64_gcc_128
-      ;;
-    arm)
-      OPENSSL_TARGET=linux-armv4
-      ;;
-    aarch64)
-      OPENSSL_TARGET=linux-aarch64
-      ;;
-  esac
-}
-
-configure_target() {
-  cd ${PKG_BUILD}/.${TARGET_NAME}
-  ./Configure ${PKG_CONFIGURE_OPTS_TARGET} ${PKG_CONFIGURE_OPTS_SHARED} ${PLATFORM_FLAGS} ${OPENSSL_TARGET} ${CFLAGS} ${LDFLAGS}
-}
-
-makeinstall_target() {
-  make DESTDIR=${INSTALL} install_sw
-  make DESTDIR=${SYSROOT_PREFIX} install_sw
-}
+. ${ROOT}/packages/security/openssl/package.mk
 
 post_makeinstall_target() {
   rm -rf ${INSTALL}/etc/ssl/misc
@@ -90,7 +19,7 @@ post_makeinstall_target() {
     ln -sf /run/rocknix/cacert.pem ${INSTALL}/etc/ssl/cacert.pem
     ln -sf /run/rocknix/cacert.pem ${INSTALL}/etc/ssl/cert.pem
 
-  # backwards comatibility
+  # backwards compatibility
   mkdir -p ${INSTALL}/etc/pki/tls
     ln -sf /run/rocknix/cacert.pem ${INSTALL}/etc/pki/tls/cacert.pem
   mkdir -p ${INSTALL}/etc/pki/tls/certs
