acef5d5656cba7926ab20133aa84019012da7efa
jpchow26 <jpchow26@gmail.com>
2025-11-26 08:23:38 -0500

    Update DuckStation version to 0.1-10130

--- projects/ROCKNIX/packages/emulators/standalone/duckstation-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/duckstation-sa/package.mk
@@ -5,7 +5,7 @@ PKG_NAME="duckstation-sa"
 PKG_LICENSE="GPLv3"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SITE="https://github.com/stenzek/duckstation"
-PKG_VERSION="0.1-9787"
+PKG_VERSION="0.1-10130"
 PKG_LONGDESC="Fast PlayStation 1 emulator for x86-64/AArch32/AArch64 "
 PKG_TOOLCHAIN="manual"
 
