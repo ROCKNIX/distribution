214e11fa479c2d9a16ff7ea04033b4cedc48859d
Bumblebrow <wesley.fielder@gmail.com>
2025-11-29 08:53:02 -0600

    xemu-sa: updated to 0.8.17

--- projects/ROCKNIX/packages/emulators/standalone/xemu-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/xemu-sa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="xemu-sa"
-PKG_VERSION="v0.8.116"
+PKG_VERSION="v0.8.117"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/xemu-project/xemu"
 PKG_URL="${PKG_SITE}/archive/refs/tags/${PKG_VERSION}.tar.gz"
