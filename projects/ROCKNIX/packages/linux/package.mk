217592abba23fdb36d4593487c4c93fee02b239f
sydarn <sydarn@proton.me>
2025-12-07 20:11:42 +0100

    rk3566: move to linux 6.18

--- projects/ROCKNIX/packages/linux/package.mk
+++ projects/ROCKNIX/packages/linux/package.mk
@@ -31,11 +31,8 @@ case ${DEVICE} in
   ;;
   *)
     case ${DEVICE} in
-      H700|SM8550|SM8650|SM8250|RK3399|S922X)
+      H700|SM8550|SM8650|SM8250|RK3399|S922X|RK3566)
         PKG_VERSION="6.18.1"
-      ;;
-      RK3566)
-        PKG_VERSION="6.17.11"
       ;;
       *)
         PKG_VERSION="6.12.61"
