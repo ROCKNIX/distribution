fb0f2c767ec44aab7e39310fb8629d7c08b83ad2
sydarn <sydarn@proton.me>
2026-02-04 19:43:08 +0100

    vulkan wsi: switch back to upstream, with ginkage patch
    But use FIFO presentation option

--- projects/ROCKNIX/packages/graphics/vulkan-wsi-layer/package.mk
+++ projects/ROCKNIX/packages/graphics/vulkan-wsi-layer/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="vulkan-wsi-layer"
-PKG_VERSION="d48decd368ecef2278a2dbf780d4ea36089e4d04"
+PKG_VERSION="cb1a50cf7e640ad7306e673131ded98c0f133628"
 PKG_LICENSE="MIT"
-PKG_SITE="https://github.com/ginkage/vulkan-wsi-layer"
-PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://gitlab.freedesktop.org/mesa/vulkan-wsi-layer"
+PKG_URL="${PKG_SITE}/-/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ${VULKAN}"
 PKG_LONGDESC="Implements Vulkan extensions for Window System Integration inside a Vulkan layer."
 PKG_TOOLCHAIN="cmake"
