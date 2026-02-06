6aef6aa9ed209e361d0e5aa8503c3ce2c4bfa1de
Bumblebrow <wesley.fielder@gmail.com>
2026-02-06 16:07:50 -0600

    hypseus-singe: update to 2.11.7

--- projects/ROCKNIX/packages/emulators/standalone/hypseus-singe/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/hypseus-singe/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="hypseus-singe"
-PKG_VERSION="1d8ad1526511d966b6b78a3149b492ca124c1a0a" # v2.11.6
+PKG_VERSION="a7ab17642f839729c68eb38f17a0374af59f11bd" # v2.11.7
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/DirtBagXon/hypseus-singe"
 PKG_URL="${PKG_SITE}.git"
