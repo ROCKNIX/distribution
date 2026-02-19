af80aa451c8e3ed5f7aae4ed99101073ce710c13
Bumblebrow <wfiel36849@peoplepc.com>
2026-02-18 22:46:09 -0600

    Update package.mk

--- projects/ROCKNIX/packages/emulators/standalone/xemu-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/xemu-sa/package.mk
@@ -3,10 +3,9 @@
 
 PKG_NAME="xemu-sa"
 PKG_VERSION="26fcbe54f17e496bdf530dad9f237e74e2f943fc"
-PKG_VERSION="v0.8.133"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/xemu-project/xemu"
-PKG_URL="${PKG_SITE}/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libthai gtk3 libsamplerate libpcap atk SDL2 Python3 zlib pixman bzip2 openssl xwayland libslirp"
 PKG_LONGDESC="Xemu - A free and open-source application that emulates the original Microsoft Xbox game console."
 PKG_TOOLCHAIN="make"
@@ -32,7 +31,7 @@ pre_configure_target() {
   export TARGET_CXXFLAGS=$(echo ${TARGET_CXXFLAGS} | sed -e "s|-DNDEBUG||g")
   export CFLAGS=$(echo ${CFLAGS} | sed -e "s|-DNDEBUG||g")
   export CXXFLAGS=$(echo ${CXXFLAGS} | sed -e "s|-DNDEBUG||g")
-  export XEMU_VERSION=${PKG_VERSION}
+
   # Required for python
   export DONT_BUILD_LEGACY_PYC=1
 
