367d6e4338e07ae470634e73a9218465de339879
Bumblebrow <wesley.fielder@gmail.com>
2026-04-25 22:21:06 -0500

    vita3k-sa: updated to 3964-703828d5

--- projects/ROCKNIX/packages/emulators/standalone/vita3k-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/vita3k-sa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2026 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="vita3k-sa"
-PKG_VERSION="9916d8655fdfdf9cc2ec40c6f3f4b142eeb31608"
+PKG_VERSION="703828d56d80f9664e86b512ef8ee0bcaacb5047"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/Vita3K/Vita3K"
 PKG_URL="${PKG_SITE}.git"
