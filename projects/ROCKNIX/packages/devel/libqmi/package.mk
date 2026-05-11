f88261d048c3646f5184cbc734e0d16a30f20db6
Gianni Spadoni <me@gio.blue>
2026-05-11 15:45:41 +0800

    libqmi: add libqmi and libqrtr-glib packages
    Adds libqmi and its dependency libqrtr-glib.
    
    Requires the libprotobuf-c package.
    
    Signed-off-by: Gianni Spadoni <me@gio.blue>

---
+++ projects/ROCKNIX/packages/devel/libqmi/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2026 ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="libqmi"
+PKG_VERSION="0536681a4ab46021e13c1cf8fa94b0b13b93b0c6"
+PKG_LICENSE="LGPL-2.1-or-later"
+PKG_SITE="https://github.com/linux-mobile-broadband/libqmi"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain glib libqrtr-glib ninja:host"
+PKG_DEPENDS_HOST="toolchain:host"
+PKG_LONGDESC="libqmi is a glib-based library for talking to WWAN modems and devices which speak the Qualcomm MSM Interface (QMI) protocol."
+PKG_TOOLCHAIN="meson"
+
+pre_configure_target() {
+  PKG_MESON_OPTS_TARGET+=" -Dbash_completion=false \
+                           -Dmbim_qmux=false \
+                           -Dman=false \
+                           -Dintrospection=false"
+}
+
+post_makeinstall_target() {
+  rm -rf ${INSTALL}/usr/include
+}
