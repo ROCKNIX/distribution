264f2b8f326e40a006705bfbd0ae03881fdb8789
Philippe Simons <simons.philippe@gmail.com>
2026-05-12 16:00:31 +0200

    Revert "commander - force zink for SM8550 to work around GPU hangs"

--- projects/ROCKNIX/packages/misc/modules/sources/commander.sh
+++ projects/ROCKNIX/packages/misc/modules/sources/commander.sh
@@ -6,9 +6,6 @@
 . /etc/profile
 set_kill set "commander"
 
-# Work around GPU hangs on SM8550
-[[ "${HW_DEVICE}" == "SM8550" ]] && export MESA_LOADER_DRIVER_OVERRIDE="zink"
-
 sway_fullscreen "commander" &
 
 /usr/bin/commander
