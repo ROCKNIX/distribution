67269047c9af40ca71acb3132bcff79c90fd0b43
bulzipke <bulzipke@naver.com>
2026-05-12 14:42:51 +0900

    Replace resume and save icons with retroarch icons

--- projects/ROCKNIX/packages/ui/lowerdeck/package.mk
+++ projects/ROCKNIX/packages/ui/lowerdeck/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2026-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="lowerdeck"
-PKG_VERSION="0ad677c37419218bc284c3aec0d30b131e8efb8c"
+PKG_VERSION="faf3cd5b259f15581dda0e2569a877f3d8d821c0"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/bulzipke/lowerdeck"
