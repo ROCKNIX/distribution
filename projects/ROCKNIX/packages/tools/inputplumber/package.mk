7d31b335ef2bb79a9d7b5f2d8fe23116a23601a6
Gianni Spadoni <me@gio.blue>
2026-05-24 17:28:10 +0800

    inputplumber: build the SSC PR branch
    We need to build InputPlumber as SSC support is still unmerged.
    
    Pretty standard Rust build.
    
    Signed-off-by: Gianni Spadoni <me@gio.blue>

--- projects/ROCKNIX/packages/tools/inputplumber/package.mk
+++ projects/ROCKNIX/packages/tools/inputplumber/package.mk
@@ -2,17 +2,31 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="inputplumber"
-PKG_VERSION="v0.75.2"
+PKG_VERSION="5ffb8b44d4daf377b9b0e1d7d1c26c65b7012d4b"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/ShadowBlip/InputPlumber"
-PKG_URL="https://github.com/ShadowBlip/InputPlumber/releases/download/${PKG_VERSION}/inputplumber-aarch64.tar.gz"
+PKG_SITE="https://github.com/gio3k/InputPlumber"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain systemd libevdev libiio polkit"
 PKG_LONGDESC="Open source input router and remapper daemon for Linux"
 PKG_TOOLCHAIN="manual"
 
+make_target() {
+  export LD_LIBRARY_PATH="${TOOLCHAIN}/lib"
+  export BINDGEN_EXTRA_CLANG_ARGS="--sysroot=${SYSROOT_PREFIX}"
+
+  cargo build \
+    --target ${TARGET_NAME} \
+    --no-default-features 
+}
+
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr
-  rsync -ar ${PKG_BUILD}/usr/ ${INSTALL}/usr/
+  mkdir -p ${INSTALL}
+    cp -a ${PKG_BUILD}/rootfs/* ${INSTALL}
+
+  mkdir -p ${INSTALL}/usr/bin
+    # TODO: change this from debug to release when this PR is ready to merge
+    # release builds take 10 minutes!
+    cp -a ${PKG_BUILD}/.${TARGET_NAME}/target/${TARGET_NAME}/debug/inputplumber ${INSTALL}/usr/bin
 }
 
 post_install() {
