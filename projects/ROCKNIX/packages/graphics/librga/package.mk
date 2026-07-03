7fe494b9eb4039149da27bb7f06f5c3126afd003
Felix Jones <felix@felixjones.co.uk>
2026-07-03 23:51:48 +0200

    librga: switch to JeffyCN linux-rga-multi RGA source

--- projects/ROCKNIX/packages/graphics/librga/package.mk
+++ projects/ROCKNIX/packages/graphics/librga/package.mk
@@ -2,11 +2,13 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="librga"
-PKG_VERSION="ccfec13d6c48e3f0c7f24810b3dac162c40cdda8"
+PKG_VERSION="57a1067a246c71fa6c9a355d1668884fda155dd5"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="Apache-2.0"
 PKG_DEPENDS_TARGET="toolchain libdrm"
-PKG_SITE="https://github.com/varphone/rockchip-linux-rga"
+PKG_SITE="https://github.com/JeffyCN/mirrors"
 PKG_URL="${PKG_SITE}.git"
+PKG_GIT_CLONE_BRANCH="linux-rga-multi"
+PKG_GIT_CLONE_SINGLE="yes"
 PKG_LONGDESC="RGA is an independent 2D hardware acceleration userspace driver"
 PKG_TOOLCHAIN="meson"
