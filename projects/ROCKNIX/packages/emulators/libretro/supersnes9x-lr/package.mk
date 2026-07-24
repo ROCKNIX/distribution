84044ce971c1d4b247562e3b720183edeacff01f
litania <litania@naver.com>
2026-07-24 10:07:25 +0900

    lr-supersnes9x: bump newer version

--- projects/ROCKNIX/packages/emulators/libretro/supersnes9x-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/supersnes9x-lr/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="supersnes9x-lr"
-PKG_VERSION="a5ca2bec6f97a85778abd6fb048567b01204753e"
+PKG_VERSION="9bd4c4268fbf340b56e04bbafaff83980d2b5be6"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/shanytc/snes9x"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
