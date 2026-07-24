2acc6bf865e631bd70f1ab61f148db86f0662151
litania <litania@naver.com>
2026-07-24 10:02:01 +0900

    lr-genesis-plus-gx: bump version to latest

--- projects/ROCKNIX/packages/emulators/libretro/genesis-plus-gx-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/genesis-plus-gx-lr/package.mk
@@ -21,7 +21,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx-lr"
-PKG_VERSION="e48a37173350b5aec2a6431add7c1a625d1f28bf"
+PKG_VERSION="fa4dca561e08d5be9077419f7b255e1da213ed21"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
--- projects/ROCKNIX/packages/emulators/libretro/genesis-plus-gx-wide-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/genesis-plus-gx-wide-lr/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx-wide-lr"
-PKG_VERSION="b7d314220d755537aebb7bc1750ac21f764365c4"
+PKG_VERSION="29d9d104338f46bc2e65438fb207bcf54f701e92"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX-Wide"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
