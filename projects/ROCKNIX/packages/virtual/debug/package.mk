563c137be31ab5838e54d7f3ad5b828f4b24c68f
sunshineinabox <aqemail@gmail.com>
2026-08-04 07:33:00 -0700

    renderdoc: re-add package with xcb dependencies

--- projects/ROCKNIX/packages/virtual/debug/package.mk
+++ projects/ROCKNIX/packages/virtual/debug/package.mk
@@ -3,7 +3,7 @@
 
 . ${ROOT}/packages/virtual/debug/package.mk
 
-PKG_DEPENDS_TARGET+=" nvtop apitrace"
+PKG_DEPENDS_TARGET+=" nvtop apitrace renderdoc"
 
 # strace is broken on 6.19 kernels, so temporarily remove
 PKG_DEPENDS_TARGET=${PKG_DEPENDS_TARGET//"strace"/}
