75db51c3da428eaf39ccbca68d3d00174f08f9be
André Braga <code@andrebraga.com>
2026-08-04 18:55:34 +0100

    SM8750: bump kernel 7.1.3 -> 7.1.8

--- projects/ROCKNIX/packages/linux/package.mk
+++ projects/ROCKNIX/packages/linux/package.mk
@@ -37,7 +37,7 @@ case ${DEVICE} in
     PKG_PATCH_DIRS+=" 7.0"
     ;;
   SM8750)
-    PKG_VERSION="7.1.3"
+    PKG_VERSION="7.1.8"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS+=" 7.0"
     ;;
