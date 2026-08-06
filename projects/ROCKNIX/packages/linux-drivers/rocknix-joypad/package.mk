a9d65dea1be731f1649922773cdb17b96409f1cb
Mike <mike.harris@gmail.com>
2026-08-06 14:29:58 -0700

    rocknix-joypad: Use new version, with newer kernel API.

--- projects/ROCKNIX/packages/linux-drivers/rocknix-joypad/package.mk
+++ projects/ROCKNIX/packages/linux-drivers/rocknix-joypad/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-joypad"
-PKG_VERSION="7647fdb0fc89cd69b284903bf7707e861df5dc7e"
+PKG_VERSION="a6b24835aa1e6339360d08baacd401bb09d08049"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/ROCKNIX/rocknix-joypad"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
