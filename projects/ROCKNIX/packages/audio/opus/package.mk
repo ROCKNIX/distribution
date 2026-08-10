591a4f5a39bf7c243dbee32088ff103fdf0a9011
spycat88 <spycat88@users.noreply.github.com>
2026-08-10 20:21:23 +0000

    opus: pin package

---
+++ projects/ROCKNIX/packages/audio/opus/package.mk
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+. ${ROOT}/packages/audio/opus/package.mk
+
+PKG_VERSION="1.5.2"
+PKG_SHA256="65c1d2f78b9f2fb20082c38cbe47c951ad5839345876e46941612ee87f9a7ce1"
+PKG_URL="https://ftp.osuosl.org/pub/xiph/releases/opus/${PKG_NAME}-${PKG_VERSION}.tar.gz"
