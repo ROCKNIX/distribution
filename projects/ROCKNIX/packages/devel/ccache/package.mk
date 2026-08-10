544d3a38f4b07c08c8c98ce637fae0d763c44306
spycat88 <spycat88@users.noreply.github.com>
2026-08-10 18:41:03 +0000

    packages: restore folder to upstream state and move local changes

--- projects/ROCKNIX/packages/devel/ccache/package.mk
+++ projects/ROCKNIX/packages/devel/ccache/package.mk
@@ -6,3 +6,26 @@
 pre_configure_host() {
   export CXXFLAGS+=" -Wno-error=restrict"
 }
+
+post_makeinstall_host() {
+  # setup ccache
+  if [ -z "${CCACHE_DISABLE}" ]; then
+    CCACHE_DIR="${BUILD_CCACHE_DIR}" ${TOOLCHAIN}/bin/ccache --max-size=${CCACHE_CACHE_SIZE}
+    CCACHE_DIR="${BUILD_CCACHE_DIR}" ${TOOLCHAIN}/bin/ccache --set-config compression_level=${CCACHE_COMPRESSLEVEL}
+    CCACHE_DIR="${BUILD_CCACHE_DIR}" ${TOOLCHAIN}/bin/ccache --set-config sloppiness=${CCACHE_SLOPPINESS}
+  fi
+
+  cat >${TOOLCHAIN}/bin/host-gcc <<EOF
+#!/bin/sh
+${TOOLCHAIN}/bin/ccache ${LOCAL_CC} "\$@"
+EOF
+
+  chmod +x ${TOOLCHAIN}/bin/host-gcc
+
+  cat >${TOOLCHAIN}/bin/host-g++ <<EOF
+#!/bin/sh
+${TOOLCHAIN}/bin/ccache ${LOCAL_CXX} "\$@"
+EOF
+
+  chmod +x ${TOOLCHAIN}/bin/host-g++
+}
