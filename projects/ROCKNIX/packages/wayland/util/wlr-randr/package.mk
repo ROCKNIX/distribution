c94cd1ec823b34562a5a6fce7e25626dc23c8a74
sunshineinabox <aqemail@gmail.com>
2026-08-09 21:21:53 -0700

    packages: drop stale shadows superseded by LibreELEC core
    cbindgen, mtdev and swaybg overrides were byte-identical to the core
    recipes apart from an older version pin; remove them. wlr-randr keeps
    its profile.d file but sources the core recipe instead of pinning.

--- projects/ROCKNIX/packages/wayland/util/wlr-randr/package.mk
+++ projects/ROCKNIX/packages/wayland/util/wlr-randr/package.mk
@@ -1,11 +1,4 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
-PKG_NAME="wlr-randr"
-PKG_VERSION="0.4.1"
-PKG_SHA256="8af085aeed3132f7442a05bafeadfd0ea30e9a4818af1a91e161f7d560f2cd93"
-PKG_LICENSE="MIT"
-PKG_SITE="https://git.sr.ht/~emersion/wlr-randr"
-PKG_URL="https://git.sr.ht/~emersion/wlr-randr/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain wayland"
-PKG_LONGDESC="Utility to manage outputs of a Wayland compositor."
+. ${ROOT}/packages/wayland/util/wlr-randr/package.mk
