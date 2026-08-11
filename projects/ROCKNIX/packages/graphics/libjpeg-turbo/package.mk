aba053411907f1aa6703ac2b3d44c1b4a8c24258
spycat88 <spycat88@users.noreply.github.com>
2026-08-11 19:49:07 +0000

    libjpeg-turbo: pin package against upstream version

--- projects/ROCKNIX/packages/graphics/libjpeg-turbo/package.mk
+++ projects/ROCKNIX/packages/graphics/libjpeg-turbo/package.mk
@@ -1,39 +1,13 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
-# Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
-PKG_NAME="libjpeg-turbo"
-PKG_VERSION="3.0.1"
-PKG_LICENSE="GPL"
-PKG_SITE="https://libjpeg-turbo.org/"
-PKG_URL="https://github.com/libjpeg-turbo/libjpeg-turbo/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="toolchain:host"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="A high-speed version of libjpeg for x86 and x86-64 processors which uses SIMD."
-PKG_BUILD_FLAGS="+pic +pic:host"
+. ${ROOT}/packages/graphics/libjpeg-turbo/package.mk
 
-PKG_CMAKE_OPTS_HOST="-DENABLE_STATIC=ON \
-                     -DENABLE_SHARED=OFF \
-                     -DWITH_JPEG8=ON \
-                     -DWITH_SIMD=OFF"
-
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-                       -DENABLE_STATIC=ON \
-                       -DENABLE_SHARED=ON \
-                       -DWITH_JPEG8=ON"
-
-if target_has_feature "(neon|sse)"; then
-  PKG_CMAKE_OPTS_TARGET+=" -DWITH_SIMD=ON"
-else
-  PKG_CMAKE_OPTS_TARGET+=" -DWITH_SIMD=OFF"
-fi
+PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET//-DENABLE_STATIC=ON/-DENABLE_STATIC=OFF}"
+PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET//-DENABLE_SHARED=OFF/-DENABLE_SHARED=ON}"
 
 if [[ ${TARGET_ARCH} =~ i*86|x86_64 ]]; then
   PKG_DEPENDS_HOST+=" nasm:host"
   PKG_DEPENDS_TARGET+=" nasm:host"
 fi
 
-post_makeinstall_target() {
-  rm -rf ${INSTALL}/usr/bin
-}
