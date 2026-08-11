19706df4bb505bc799f997ac7a6808ccabf2df32
sunshineinabox <aqemail@gmail.com>
2026-08-11 14:41:26 -0700

    libevdev: update pkg url
    plain http now 302s and the fetch fails, while https serves the file
    directly. The tarball never moved 1.13.6 is still in the directory
    listing.

---
+++ projects/ROCKNIX/packages/sysutils/libevdev/package.mk
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+. ${ROOT}/packages/sysutils/libevdev/package.mk
+
+PKG_URL="https://www.freedesktop.org/software/libevdev/${PKG_NAME}-${PKG_VERSION}.tar.xz"
