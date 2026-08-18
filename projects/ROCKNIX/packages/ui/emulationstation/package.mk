27b4e05b5eab583d0676e762929b08d131f120b8
spycat88 <spycat88@users.noreply.github.com>
2026-08-18 11:11:51 +0000

    emulationstation: bump package and enable GLES3 support

--- projects/ROCKNIX/packages/ui/emulationstation/package.mk
+++ projects/ROCKNIX/packages/ui/emulationstation/package.mk
@@ -2,43 +2,30 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="3ca0e9ad196d18b2de9c8eabad34ed21e72f5848"
+PKG_VERSION="9d664e2ffa75d0fcfd129c6598318c61b57ff8f7"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/ROCKNIX/emulationstation-next"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="boost toolchain SDL2 freetype curl freeimage bash rapidjson SDL2_mixer fping p7zip alsa vlc drm_tool pugixml"
+PKG_DEPENDS_TARGET="boost toolchain SDL2 freetype curl freeimage bash rapidjson SDL2_mixer fping p7zip alsa vlc drm_tool pugixml ${OPENGLES}"
 PKG_NEED_UNPACK="busybox"
 PKG_LONGDESC="Emulationstation emulator frontend"
 PKG_BUILD_FLAGS="-gold"
-
-PKG_PATCH_DIRS+="${DEVICE}"
-
-if [ ! "${OPENGL}" = "no" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGL} glu"
-  PKG_CMAKE_OPTS_TARGET+=" -DGL=1"
-fi
-
-if [ ! "${OPENGLES_SUPPORT}" = no ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
-  PKG_CMAKE_OPTS_TARGET+=" -DGLES2=1"
-fi
 
 PKG_CMAKE_OPTS_TARGET+=" -DROCKNIX=1 \
                          -DDISABLE_KODI=1 \
                          -DENABLE_FILEMANAGER=0 \
                          -DCEC=0 \
                          -DENABLE_PULSE=1 \
-                         -DUSE_SYSTEM_PUGIXML=1"
+                         -DUSE_SYSTEM_PUGIXML=1 \
+                         -DGLES3=1"
 
 [ "${DEVICE}" = "S922X" ] && PKG_CMAKE_OPTS_TARGET+=" -DBATTERYPLUS=1"
 
 pre_configure_target() {
   for key in SCREENSCRAPER_DEV_LOGIN \
-        GAMESDB_APIKEY \
-        CHEEVOS_DEV_LOGIN
-  do
-    if [ -z "${!key}" ]
-    then
+             GAMESDB_APIKEY \
+             CHEEVOS_DEV_LOGIN; do
+    if [ -z "${!key}" ]; then
       echo "WARNING: ${key} not declared, will not build support."
     else
       echo "USING: ${key} = ${!key}"
@@ -50,42 +37,41 @@ pre_configure_target() {
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/config/locale
-  cp -rf ${PKG_BUILD}/locale/lang/* ${INSTALL}/usr/config/locale/
+    cp -a ${PKG_BUILD}/locale/lang/* ${INSTALL}/usr/config/locale
 
-  # Pre-generate default (en_US.UTF-8) locale for lower-end devices to speed up first boot
-  # This saves a minute or two on RK3326 in a cost of about 1 MB of SYSTEM size
-  # Copy-paste of a locale generating part of es_settings script
-  I18NPATH=$(get_install_dir glibc)/usr/share/i18n/locales/ \
-    localedef --force --verbose --inputfile=en_US --charmap=UTF-8 \
-    ${INSTALL}/usr/config/locale/en_US.UTF-8 || true
+    # Pre-generate default (en_US.UTF-8) locale for lower-end devices to speed up first boot
+    # This saves a minute or two on RK3326 in a cost of about 1 MB of SYSTEM size
+    # Copy-paste of a locale generating part of es_settings script
+    I18NPATH=$(get_install_dir glibc)/usr/share/i18n/locales/ \
+      localedef --force --verbose --inputfile=en_US --charmap=UTF-8 \
+      ${INSTALL}/usr/config/locale/en_US.UTF-8 || true
+
+  mkdir -p ${INSTALL}/usr/config/emulationstation
+    cp -a ${PKG_DIR}/config/common/*.cfg ${INSTALL}/usr/config/emulationstation
+    rm -f ${INSTALL}/usr/config/emulationstation/resources/logo.png
 
   mkdir -p ${INSTALL}/usr/config/emulationstation/resources
-  cp -rf ${PKG_BUILD}/resources/* ${INSTALL}/usr/config/emulationstation/resources/
-  rm -rf ${INSTALL}/usr/config/emulationstation/resources/logo.png
+    cp -a ${PKG_BUILD}/resources/* ${INSTALL}/usr/config/emulationstation/resources
 
   mkdir -p ${INSTALL}/usr/bin
-  cp ${PKG_BUILD}/es_settings ${INSTALL}/usr/bin
-  chmod 0755 ${INSTALL}/usr/bin/es_settings
-
-  cp ${PKG_BUILD}/start_es.sh ${INSTALL}/usr/bin
-  chmod 0755 ${INSTALL}/usr/bin/start_es.sh
-
-  cp ${PKG_BUILD}/serial_number_check ${INSTALL}/usr/bin
-  chmod 0755 ${INSTALL}/usr/bin/serial_number_check
+    cp -a ${PKG_BUILD}/es_settings ${INSTALL}/usr/bin
+    cp -a ${PKG_BUILD}/start_es.sh ${INSTALL}/usr/bin
+    cp -a ${PKG_BUILD}/serial_number_check ${INSTALL}/usr/bin
 
   mkdir -p ${INSTALL}/usr/bin
-  #ln -sf /storage/.config/emulationstation/resources ${INSTALL}/usr/bin/resources
-  cp -rf ${PKG_BUILD}/emulationstation ${INSTALL}/usr/bin
+    #ln -sf /storage/.config/emulationstation/resources ${INSTALL}/usr/bin/resources
+    cp -a ${PKG_BUILD}/emulationstation ${INSTALL}/usr/bin
 
-  mkdir -p ${INSTALL}/etc/emulationstation/
-  ln -sf /storage/.config/emulationstation/themes ${INSTALL}/etc/emulationstation/
+  mkdir -p ${INSTALL}/etc
+    ln -sf /storage/.cache/system_timezone ${INSTALL}/etc/timezone
 
-  cp -rf ${PKG_DIR}/config/common/*.cfg ${INSTALL}/usr/config/emulationstation
+  mkdir -p ${INSTALL}/etc/emulationstation
+    ln -sf /storage/.config/emulationstation/themes ${INSTALL}/etc/emulationstation/themes
+    ln -sf ${INSTALL}/usr/config/emulationstation/es_systems.cfg ${INSTALL}/etc/emulationstation/es_systems.cfg
+
 
   # If we're not an emulation device, ES may still be installed so we need a default config.
-  if [ "${EMULATION_DEVICE}" = "no" ] || \
-     [ "${BASE_ONLY}" = "true" ]
-  then
+  if [[ "${EMULATION_DEVICE}" == "no" || "${BASE_ONLY}" == "true" ]]; then
     cat <<EOF >${INSTALL}/usr/config/emulationstation/es_systems.cfg
 <?xml version="1.0" encoding="UTF-8"?>
 <systemList>
@@ -105,24 +91,20 @@ makeinstall_target() {
 EOF
   fi
 
-  ln -sf ${INSTALL}/usr/config/emulationstation/es_systems.cfg ${INSTALL}/etc/emulationstation/es_systems.cfg
-
-  ln -sf /storage/.cache/system_timezone ${INSTALL}/etc/timezone
-
   #Delete all vulkan options from es_features when vulkan is not present
-  if [ ! "${VULKAN_SUPPORT}" = "yes" ]
-    then
-      xmlstarlet ed --inplace -d '//choice[contains(@name, "vulkan")]' ${INSTALL}/usr/config/emulationstation/es_features.cfg
+  if [ ! "${VULKAN_SUPPORT}" = "yes" ]; then
+    xmlstarlet ed --inplace -d '//choice[contains(@name, "vulkan")]' ${INSTALL}/usr/config/emulationstation/es_features.cfg
   fi
 }
 
 
 post_install() {
   mkdir -p ${INSTALL}/usr/share
-  ln -sf /storage/.config/locale ${INSTALL}/usr/share/locale
+    ln -sf /storage/.config/locale ${INSTALL}/usr/share/locale
 
   mkdir -p ${INSTALL}/usr/lib
-  ln -sf /usr/share/locale ${INSTALL}/usr/lib/locale
+    ln -sf /usr/share/locale ${INSTALL}/usr/lib/locale
 
-  ln -sf /usr/share/locale  ${INSTALL}/usr/config/emulationstation/locale
+  mkdir -p ${INSTALL}/usr/config/emulationstation
+    ln -sf /usr/share/locale  ${INSTALL}/usr/config/emulationstation/locale
 }
