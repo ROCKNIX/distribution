65be7b6820c84bcf0da8d8ebf1d30139bac5258b
spycat88 <spycat88@users.noreply.github.com>
2026-08-22 22:44:45 +0000

    gzdoom-sa: cleanup package

--- projects/ROCKNIX/packages/emulators/standalone/gzdoom-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/gzdoom-sa/package.mk
@@ -7,56 +7,53 @@ PKG_VERSION="g4.14.2"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/ZDoom/gzdoom"
 PKG_URL="https://github.com/ZDoom/gzdoom/archive/refs/tags/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="toolchain SDL2:host zmusic:host libvpx:host libwebp:host"
-PKG_DEPENDS_TARGET="toolchain SDL2 gzdoom-sa:host zmusic libvpx libwebp"
+PKG_DEPENDS_HOST="toolchain:host SDL2:host zmusic:host libvpx:host libwebp:host"
+PKG_DEPENDS_TARGET="toolchain gzdoom-sa:host SDL2 zmusic libvpx libwebp"
 PKG_LONGDESC="GZDoom is a modder-friendly OpenGL and Vulkan source port based on the DOOM engine"
 PKG_TOOLCHAIN="cmake-make"
 
 pre_configure_host() {
   unset HOST_CMAKE_OPTS
-  PKG_CMAKE_OPTS_HOST+=" -DZMUSIC_LIBRARIES=${TOOLCHAIN}/usr/lib/libzmusic.so \
-                        -DZMUSIC_INCLUDE_DIR=${TOOLCHAIN}/usr/include \
-                        -DNO_GTK=ON \
-                        -DHAVE_VULKAN=OFF \
-                        -DHAVE_GLES2=OFF"
-}
-
-makeinstall_host() {
-  :
+  PKG_CMAKE_OPTS_HOST="-DZMUSIC_LIBRARIES=${TOOLCHAIN}/usr/lib/libzmusic.so \
+                       -DZMUSIC_INCLUDE_DIR=${TOOLCHAIN}/usr/include \
+                       -DNO_GTK=ON \
+                       -DHAVE_VULKAN=OFF \
+                       -DHAVE_GLES2=OFF"
 }
 
 pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+                         -DFORCE_CROSSCOMPILE=ON \
+                         -DIMPORT_EXECUTABLES=${PKG_BUILD}/.${HOST_NAME}/ImportExecutables.cmake \
+                         -DZMUSIC_LIBRARIES=${SYSROOT_PREFIX}/usr/lib/libzmusic.so \
+                         -DZMUSIC_INCLUDE_DIR=${SYSROOT_PREFIX}/usr/include \
+                         -DNO_GTK=ON"
 
-  PKG_CMAKE_OPTS_TARGET+=" -DNO_GTK=ON \
-                           -DFORCE_CROSSCOMPILE=ON \
-                           -DIMPORT_EXECUTABLES=${PKG_BUILD}/.${HOST_NAME}/ImportExecutables.cmake \
-                           -DCMAKE_BUILD_TYPE=Release \
-                           -DZMUSIC_LIBRARIES=${SYSROOT_PREFIX}/usr/lib/libzmusic.so -DZMUSIC_INCLUDE_DIR=${SYSROOT_PREFIX}/usr/include"
   ### Enable GLES on devices that don't support OpenGL.
-  if [ ! "${OPENGL_SUPPORT}" = "yes" ] || [ ${PREFER_GLES} = "yes" ]
-  then
+  if [ ! "${OPENGL_SUPPORT}" = "yes" ] || [ ${PREFER_GLES} = "yes" ]; then
     PKG_CMAKE_OPTS_TARGET+=" -DHAVE_GLES2=ON"
   fi
 
   ### Enable vulkan support on devices that have it available.
-  if [ "${VULKAN_SUPPORT}" = "yes" ]
-  then
+  if [ "${VULKAN_SUPPORT}" = "yes" ]; then
     PKG_CMAKE_OPTS_TARGET+=" -DHAVE_VULKAN=ON"
   else
     PKG_CMAKE_OPTS_TARGET+=" -DHAVE_VULKAN=OFF"
   fi
 }
 
-makeinstall_target() {
+makeinstall_host() {
+  :
+}
 
+makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
-  cp ${PKG_BUILD}/.${TARGET_NAME}/gzdoom ${INSTALL}/usr/bin
-  cp ${PKG_DIR}/scripts/start_gzdoom.sh ${INSTALL}/usr/bin/
-  chmod +x ${INSTALL}/usr/bin/*
+    cp -a ${PKG_BUILD}/.${TARGET_NAME}/gzdoom ${INSTALL}/usr/bin
+    cp -a ${PKG_DIR}/scripts/start_gzdoom.sh ${INSTALL}/usr/bin
 
   mkdir -p ${INSTALL}/usr/config/gzdoom
-  cp ${PKG_DIR}/config/* ${INSTALL}/usr/config/gzdoom
-  cp ${PKG_BUILD}/.${TARGET_NAME}/*.pk3 ${INSTALL}/usr/config/gzdoom
-  cp -r ${PKG_BUILD}/.${TARGET_NAME}/soundfonts ${INSTALL}/usr/config/gzdoom
-  cp -r ${PKG_BUILD}/.${TARGET_NAME}/fm_banks ${INSTALL}/usr/config/gzdoom
+    cp -a ${PKG_DIR}/config/* ${INSTALL}/usr/config/gzdoom
+    cp -a ${PKG_BUILD}/.${TARGET_NAME}/*.pk3 ${INSTALL}/usr/config/gzdoom
+    cp -a ${PKG_BUILD}/.${TARGET_NAME}/soundfonts ${INSTALL}/usr/config/gzdoom
+    cp -a ${PKG_BUILD}/.${TARGET_NAME}/fm_banks ${INSTALL}/usr/config/gzdoom
 }
