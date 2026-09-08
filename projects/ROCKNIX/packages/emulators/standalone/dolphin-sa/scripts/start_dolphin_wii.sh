9d98367f5d94127f8130d1ced3de2d5bb1ecec38
John Williams <porschemad911@gmail.com>
2026-09-08 22:05:10 +1000

    dolphin-sa: add Sync GPU Thread ES setting

--- projects/ROCKNIX/packages/emulators/standalone/dolphin-sa/scripts/start_dolphin_wii.sh
+++ projects/ROCKNIX/packages/emulators/standalone/dolphin-sa/scripts/start_dolphin_wii.sh
@@ -124,6 +124,7 @@ EFBACCESS=$(get_setting skip_efb_cpu_access "${PLATFORM}" "${GAME}")
 EFBTEXTURE=$(get_setting store_efb_to_texture_only "${PLATFORM}" "${GAME}")
 XFBTEXTURE=$(get_setting store_xfb_to_texture_only "${PLATFORM}" "${GAME}")
 TEXTURE_CACHE_ACCURACY=$(get_setting texture_cache_accuracy "${PLATFORM}" "${GAME}")
+SYNC_GPU_THREAD=$(get_setting sync_gpu_thread "${PLATFORM}" "${GAME}")
 RUMBLE=$(get_setting rumble "${PLATFORM}" "${GAME}")
 WHACK=$(get_setting widescreen_hack "${PLATFORM}" "${GAME}")
 WPC=$(get_setting write_protect_configs "${PLATFORM}" "${GAME}")
@@ -233,6 +234,13 @@ fi
     sed -i '/GFXBackend/c\GFXBackend = OGL' ${CONF_DIR}/${DOLPHIN_INI}
   else
     sed -i '/GFXBackend/c\GFXBackend = @GRENDERER@' ${CONF_DIR}/${DOLPHIN_INI}
+  fi
+
+  # Sync GPU thread
+  if [ "$SYNC_GPU_THREAD" = "true" ]; then
+    sed -i '/SyncGPU =/c\SyncGPU = True' ${CONF_DIR}/${DOLPHIN_INI}
+  else
+    sed -i '/SyncGPU =/c\SyncGPU = False' ${CONF_DIR}/${DOLPHIN_INI}
   fi
 
   # Internal Resolution
@@ -450,6 +458,7 @@ fi
   echo "EFBTEXTURE set to: ${EFBTEXTURE}"
   echo "XFBTEXTURE set to: ${XFBTEXTURE}"
   echo "TEXTURE_CACHE_ACCURACY set to: ${TEXTURE_CACHE_ACCURACY}"
+  echo "SYNC_GPU_THREAD set to: ${SYNC_GPU_THREAD}"
   echo "RUMBLE set to: ${RUMBLE}"
   echo "WHACK set to: ${WHACK}"
   echo "WPC set to: ${WPC}"
