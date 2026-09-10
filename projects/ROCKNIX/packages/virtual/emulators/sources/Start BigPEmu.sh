512d9bec115d4ba367b5e0f0c0d0a340985d704e
John Williams <porschemad911@gmail.com>
2026-09-10 12:53:22 +1000

    bigpemu: libmali gl4es

--- projects/ROCKNIX/packages/virtual/emulators/sources/Start BigPEmu.sh
+++ projects/ROCKNIX/packages/virtual/emulators/sources/Start BigPEmu.sh
@@ -7,6 +7,11 @@ source /etc/profile
 
 set_kill set "bigpemu"
 
+# libmali GPU driver needs gl4es
+if [ -x "/usr/bin/gpudriver" ] && [ $(/usr/bin/gpudriver) = "libmali" ]; then
+  export LD_PRELOAD=/usr/lib/gl4es/libGL.so.1
+fi
+
 sway_fullscreen "bigpemu" &
 
 /usr/share/bigpemu/bigpemu >/dev/null 2>&1
