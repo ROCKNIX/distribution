321612c53e24b8a4d8434419ede5056e86f7a82e
Jacob Cook <jacobmcook1984@gmail.com>
2026-09-24 22:34:24 -0400

    AMD64: fix the build and move to Linux 7.2
    - linux: build AMD64 with 7.2, like the other current devices
    - ryzensmu: switch to the maintained fork, which builds on 7.2
    - amiberry: build upstream's x86-64 target on x86_64
    - yabasanshiro-sa: add the x86_64 port configuration
    - drop ARMSX2 from AMD64; it is meant for ARM only
    - intel-ucode 20260812, pciutils 3.15.0 and strace 6.19 on AMD64

--- projects/ROCKNIX/packages/virtual/emulators/package.mk
+++ projects/ROCKNIX/packages/virtual/emulators/package.mk
@@ -95,7 +95,7 @@ case "${DEVICE}" in
     ;;
   AMD64)
     PKG_EMUS+=" ares-sa azahar-sa cemu-sa dolphin-sa gopher64-sa mednafen melonds-sa nanoboyadvance-sa \
-                xemu-sa skyemu-sa vita3k-sa armsx2-sa"
+                xemu-sa skyemu-sa vita3k-sa"
     LIBRETRO_CORES+=" beetle-psx-lr beetle-saturn-lr bsnes-lr bsnes-hd-lr dolphin-lr"
 esac
 
@@ -1184,11 +1184,6 @@ makeinstall_target() {
 
   ### Sony Playstation 2
   case ${DEVICE} in
-  AMD64)
-    add_emu_core ps2 armsx2 armsx2-sa true
-    install_script "Start ARMSX2.sh"
-    add_es_system ps2
-    ;;
   RK3399|RK3576|RK3566|RK3588|SM4450|SM6115|SM8250|SM8550|SM8650|SM8750|S922X)
     add_emu_core ps2 aethersx2 aethersx2-sa true
     case ${DEVICE} in
